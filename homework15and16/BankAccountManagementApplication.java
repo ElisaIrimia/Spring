a639fca55dc8971ccb9b8c9607a86ef919ae09d2
ElisaIrimia <elisafilip25@gmail.com>
2026-08-04 14:50:25 +0300

    Add files via upload

---
+++ homework15and16/BankAccountManagementApplication.java
@@ -0,0 +1,11 @@
+package com.example.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountManagementApplication.class, args);
+    }
+}
